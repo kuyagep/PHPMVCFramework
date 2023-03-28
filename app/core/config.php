8f5fd2f08ef7993af7af94a56be5837067ac9330
Geperson Mamalias <125843365+kuyagep@users.noreply.github.com>
2023-03-28 21:01:30 +0800

    commit update

--- app/core/config.php
+++ app/core/config.php
@@ -17,12 +17,12 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
 	define('DB_PASS', '');
 	define('DB_DRIVER', '');
 	
-	define('ROOT', 'http://localhost/mvc/public');
+	define('ROOT', 'http://localhost/phpmvcframework/public');
 
 }else
 {
 	/** database config **/
-	define('DBNAME', 'my_db');
+	define('DBNAME', 'framework_db');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
